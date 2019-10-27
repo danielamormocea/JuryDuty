ee8f5358e07ac8b93340d0934d9e9fb2e8544b8e
Daniela Mormocea <daniela.mormocea@gmail.com>
2019-10-27 22:39:44 +0200

    create database with contestants

--- app/models.py
+++ app/models.py
@@ -8,7 +8,8 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(1000))
     type_user = db.Column(db.Integer) # 0 - admin, 1 - organizator 2 - juriu
 
-#Tabel Concurenti:
+
+#Tabel Concurent:
     # Nume
     # Descriere
     # Varsta
@@ -16,4 +17,19 @@ class User(UserMixin, db.Model):
     # Runda concurs
     # Nota
     # still in concurs sau nu
+
+class Contestant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000), unique=True)
+    age = db.Column(db.Integer)
+    description = db.Column(db.String(1000))
+    round_no = db.Column(db.Integer)
+    series_no = db.Column(db.Integer)
+    grade = db.Column(db.Integer)
+
+
+
+
+
+    
 
--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,60 @@
 #from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from flask import Blueprint
 from flask_login import login_required, current_user
-
+from .models import User, Contestant
 from . import db
 
 routes = Blueprint('main', __name__)
 
+contest_name = ""
+contest_rounds = 0
+contest_series = 0
+i = 0
+
 @routes.route('/')
 def index():
-    return render_template('index.html', list = )
+    return render_template('index.html')
 
 @routes.route('/profile')
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+
+@routes.route ('/organize', methods=['GET'])
+def organize():
+    return render_template('organize.html')
+
+@routes.route ('/organize', methods=['POST'])
+def organize_post():
+    contest_name = request.form.get('contest_name')
+    contest_rounds = request.form.get('round_no')
+    contest_series = request.form.get('series_no')
+    return redirect(url_for('main.add_contestant'))
+
+@routes.route('/add_contestant' , methods=['GET'])
+def add_contestant():
+    return render_template('add_contestant.html')
+
+
+@routes.route('/add_contestant' , methods=['POST'])
+def add_contestant_post():
+    global i
+    contestant_name = request.form.get('name_contestant')
+    age = request.form.get('age')
+    description = request.form.get('description')
+    i += 1
+    new_contestant = Contestant( id = i, name = contestant_name, age = age, description = description, round_no = 0, series_no = 0, grade = 0)
+
+    db.session.add(new_contestant)
+    db.session.commit()
+
+    return redirect(url_for('main.add_contestant_post'))
+
+
+    
+
+
 
 
 
